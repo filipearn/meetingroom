d15953a0ec698481963e60212e3d591c7aa1ee81
Filipe Nepomuceno <filipearn@yahoo.com.br>
2021-08-03 16:10:06 -0300

    Changes on configurations

--- src/main/java/one/innovation/digital/meetingroom/controller/RoomController.java
+++ src/main/java/one/innovation/digital/meetingroom/controller/RoomController.java
@@ -13,8 +13,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController
-@CrossOrigin(origins = "http://localhost:4200")
+
+@RestController @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/rooms")
 public class RoomController {
 
